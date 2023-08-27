f58eac825a332c7ee079de1d0a97af35c691e978
TJ Helm <thelm@nait.ca>
2023-08-27 16:38:03 -0600

    Updated parent/child node relationships with shared pointers.

--- src/engine.cpp
+++ src/engine.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <string.h>
 #include <memory>
+#include <algorithm>
 #include <type_traits>
 #include <tuple>
 
@@ -12,19 +13,142 @@ constexpr auto SCREEN_WIDTH  = 800;
 constexpr auto SCREEN_HEIGHT = 450;
 
 class Engine;
+class Node;
+
+typedef std::shared_ptr<Node> shared_node_ptr;
+typedef std::weak_ptr<Node> node_ptr;
 
 class Node
 {
 public:
+    node_ptr self;
+
+    node_ptr root;
+
+    node_ptr parent;
+    std::vector<shared_node_ptr> children;
+
+    std::string name;
+
     Node(const char* p_name) : name(p_name) {}
 
     virtual void _ready() {}
     virtual void _update() {}
     virtual void _draw() {}
 
-    std::shared_ptr<Node> parent;
+    Node& getNode(const char* path)
+    {
+        std::vector<std::string> tokens = std::vector<std::string>();
 
-    std::string name;
+        std::string current {""};
+
+        const char* c = &path[0];
+        while (*c != '\0')
+        {
+            bool nextIsEnd = *(c+1) == '\0';
+            if (*c == '/' || nextIsEnd)
+            {
+                if (nextIsEnd)
+                    current.push_back(*c);
+                if (current != "")
+                {
+                    tokens.push_back(current);
+                    current = {""};
+                }
+            }
+            else
+            {
+                current.push_back(*c);
+            }
+
+            ++c;
+        }
+
+        node_ptr currentNode;
+        // Determine starting node.
+        if (tokens[0] == "root")
+        {
+            currentNode = root;
+            tokens.erase(tokens.begin());
+        }
+        else
+        {
+            currentNode = shared_node_ptr(this);
+        }
+
+        // Follow token path.
+        for (std::string& token : tokens)
+        {
+            currentNode = getNodeByToken(currentNode, token);
+
+            if (currentNode.expired())
+            {
+                // Break early and the function will return the expired pointer.
+                std::cerr << "Invalid token in getNode, fix path. (" + token + ")";
+                break;
+            }
+        }
+
+        return *currentNode.lock();
+    }
+
+    void addChild(node_ptr child)
+    {
+        if (auto child_ptr = child.lock())
+        {
+            // Remove this child from current parent.
+            child_ptr->removeParent();
+
+            // Add as child to this.
+            children.push_back(child_ptr);
+
+            // Set parent to new parent.
+            child_ptr->parent = self;
+        }
+        else
+        {
+            std::cerr << "Couldn't add invalid child." << std::endl;
+        }
+    }
+private:
+    node_ptr getNodeByToken(node_ptr currentNode, std::string& token)
+    {
+        if (auto currentNode_ptr = currentNode.lock())
+        {
+            if (token == "..")
+            {
+                return currentNode_ptr->parent;
+            }
+            else
+            {
+                // Loop through children to find by name.
+                for (auto child : currentNode_ptr->children)
+                {
+                    if (child->name == token)
+                    {
+                        return child;
+                    }
+                }
+            }
+        }
+
+        return node_ptr();
+    }
+
+    void removeParent()
+    {
+        if (auto parent_ptr = parent.lock())
+        {
+            auto captureName = name;
+            auto at = std::remove_if(parent_ptr->children.begin(),
+                                            parent_ptr->children.end(),
+                                            [captureName](shared_node_ptr& node) {
+                                                return node->name == captureName;
+                                            });
+
+            parent_ptr->children.erase(at);
+        }
+    }
 };
 
 class Resource
@@ -50,9 +174,11 @@ public:
 
         singleton = std::make_unique<Engine>();
 
-        SetTraceLogLevel(3);
+        SetTraceLogLevel(4);
         InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Window title");
         SetTargetFPS(60);
+
+        root = std::make_shared<Node>("root");
 
         return *singleton;
     }
@@ -61,27 +187,19 @@ public:
     {
         started = true;
 
-        for (auto& node : nodes)
-        {
-            node->_ready();
-        }
+        // Ready
+        recursiveRun(root, [](shared_node_ptr node){ node->_ready(); });
 
         while (!WindowShouldClose())
         {
-            // Updating
-            for (auto& node : nodes)
-            {
-                node->_update();
-            }
+            // Update
+            recursiveRun(root, [](shared_node_ptr node){ node->_update(); });
 
             BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
-            for (auto& node : nodes)
-            {
-                node->_draw();
-            }
+            recursiveRun(root, [](shared_node_ptr node){ node->_draw(); });
 
             EndDrawing();
         }
@@ -101,9 +219,7 @@ public:
     {
         static_assert(std::is_base_of<Resource, T>::value, "T must derive from Resource.");
 
-        resources.push_back(std::make_shared<T>(resource));
-
-        resources[resources.size()-1]->_load();
+        LoadResource(std::make_shared<T>());
 
         return *this;
     }
@@ -121,54 +237,50 @@ public:
     }
 
     template <typename T>
-    Engine& addNode(T node)
+    Engine& addNodeRoot(T node)
     {
         static_assert(std::is_base_of<Node, T>::value, "T must derive from Node.");
 
-        nodes.push_back(std::make_shared<T>(node));
+        root->addChild(std::make_shared<T>(node));
+
+        auto& childPtr = root->children.back();
+
+        childPtr->root = root;
+        childPtr->self = childPtr;
 
         if (started)
         {
-            nodes[nodes.size()-1]->_ready();
+            childPtr->_ready();
         }
 
         return *this;
     }
 
-    static void ParentNode(Node* child, const char* parentName)
-    {
-        child->parent = FindNode(parentName);
-    }
-
-    static std::shared_ptr<Node> FindNode(const char* name)
-    {
-        for (auto node : nodes)
-        {
-            if (strcmp(node->name.c_str(), name) == 0)
-            {
-                return node;
-            }
-        }
-
-        return nullptr;
-    }
-
     template <typename T, class... Args>
-    static void println(T t, Args... args)
+    static void print(T t, Args... args)
     {
         std::cout << t << std::endl;
     }
 
     template <typename T, class... Args>
-    static void print(T t, Args... args)
+    static void printerr(T t, Args... args)
     {
-        std::cout << t;
+        std::cerr << t << std::endl;
     }
 private:
     inline static bool started = false;
 
     inline static std::vector<std::shared_ptr<Resource>> resources;
-    inline static std::vector<std::shared_ptr<Node>> nodes;
+    inline static std::shared_ptr<Node> root;
+
+    static void recursiveRun(shared_node_ptr node, void (*function)(shared_node_ptr))
+    {
+        function(node);
+        for (auto child : node->children)
+        {
+            recursiveRun(child, function);
+        }
+    }
 };
 
 class EngineTransform
@@ -207,27 +319,33 @@ public:
 class EngineTextureRect : public Node, public EngineTransform, public ColorModulator
 {
 public:
-    EngineTextureRect(const char* name, std::shared_ptr<EngineTexture> p_texture) : Node(name), texture(p_texture) {}
+    EngineTextureRect(const char* name, std::weak_ptr<EngineTexture> p_texture) : Node(name), texture(p_texture) {}
     EngineTextureRect(const char* name) : Node(name) {}
 
-    std::shared_ptr<EngineTexture> texture;
+    std::weak_ptr<EngineTexture> texture;
 
     void _ready() override
     {
-        position = {
-            SCREEN_WIDTH / 2.0f - texture->texture.width / 2.0f,
-            SCREEN_HEIGHT / 2.0f - texture->texture.height / 2.0f,
-            0.0
-        };
+        if (auto texture = this->texture.lock())
+        {
+            position = {
+                SCREEN_WIDTH / 2.0f - texture->texture.width / 2.0f,
+                SCREEN_HEIGHT / 2.0f - texture->texture.height / 2.0f,
+                0.0
+            };
+        }
     }
 
     void _draw() override
     {
-        DrawTexturePro(texture->texture,
-        {0, 0, texture->texture.width * 1.0f, texture->texture.height * 1.0f},
-        {position.x, position.y, texture->texture.width * scale.x, texture->texture.height * scale.y},
-        {0, 0},
-        QuaternionToEuler(rotation).z,
-        modulate);
+        if (auto texture = this->texture.lock())
+        {
+            DrawTexturePro(texture->texture,
+            {0, 0, texture->texture.width * 1.0f, texture->texture.height * 1.0f},
+            {position.x, position.y, texture->texture.width * scale.x, texture->texture.height * scale.y},
+            {0, 0},
+            QuaternionToEuler(rotation).z,
+            modulate);
+        }
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,10 @@ class CustomTexture : public EngineTextureRect
 public:
     CustomTexture(const char* name, std::shared_ptr<EngineTexture> texture) : EngineTextureRect(name, texture) {}
 
-    void _ready()
+    void _ready() override
     {
-        Engine::ParentNode(this, "Game");
+        EngineTextureRect::_ready();
+        getNode("/root/Game").addChild(self);
     }
 };
 
@@ -17,9 +18,9 @@ int main()
 {
     auto texture = std::make_shared<EngineTexture>(EngineTexture("Texture", ASSETS_PATH"test.png"));
     Engine::create()
-        .addNode(Node("Game"))
+        .addNodeRoot(Node("Game"))
         .loadResource(texture)
-        .addNode(CustomTexture("TextureRect", texture))
+        .addNodeRoot(CustomTexture("TextureRect", texture))
         .run();
     return 0;
 }
