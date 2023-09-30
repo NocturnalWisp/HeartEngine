cc58c01a29ac068e5702faef8b308a91cf9361cd
TJ Helm <thelm@nait.ca>
2023-09-30 09:14:40 -0600

    Created module generator.

---
+++ tools/ModuleGenerator.py
@@ -0,0 +1,82 @@
+# Generates a module from a provided module name and class name
+#   in the modules directory.
+
+from sys import argv
+import os
+from os.path import join
+from string import Formatter
+
+if len(argv) < 2:
+    raise Exception("Expected 2 arguments.")
+
+if not isinstance(argv[1], str):
+    raise Exception("First argument provided is not of type string. (For module name.)")
+
+if not isinstance(argv[2], str):
+    raise Exception("Second argument provided is not of type string. (For class name.)")
+
+modulesDir = "../modules"
+moduleName = argv[1]
+className = argv[2]
+
+os.makedirs(join(modulesDir, moduleName, "include", "module"), exist_ok=True)
+os.makedirs(join(modulesDir, moduleName, "src"), exist_ok=True)
+
+headerContents = '''#pragma once
+
+#include <sol/sol.hpp>
+
+#include "heart/module.h"
+
+namespace HeartEngine {{ class Engine; }}
+
+namespace HeartModules
+{{
+class {0} : public HeartEngine::Module
+{{
+public:
+    {0}(bool includeSetting = true)
+        : includeSetting(includeSetting) {{}}
+
+    void registerTypes(HeartEngine::Engine& engine, sol::state& lua) override
+    {{
+        if (includeSetting)
+            Setup(engine, lua);
+    }}
+
+    void Setup(HeartEngine::Engine& engine, sol::state& lua);
+private:
+    bool includeSetting;
+}};
+}}'''.format(className)
+
+with (open(join(modulesDir, moduleName, "include", "module", moduleName + ".h"), "w")) as header:
+    header.writelines(headerContents)
+
+sourceContents = '''#include "module/{0}.h"
+
+#include "heart/engine.h"
+
+using namespace HeartEngine;
+
+namespace HeartModules
+{{
+void {1}::Setup(HeartEngine::Engine& engine, sol::state& lua)
+{{
+    //TODO:
+}}
+}}'''.format(moduleName, className)
+
+with (open(join(modulesDir, moduleName, "src", moduleName + ".cpp"), "w")) as source:
+    source.writelines(sourceContents)
+
+cmakeContents = '''cmake_minimum_required(VERSION 3.5)
+project(heart_{0} VERSION 1.0.0)
+
+include(../ModuleHelper.cmake)
+
+include_directories(include)
+add_library(heart_{0} src/{0}.cpp)'''.format(moduleName)
+
+with (open(join(modulesDir, moduleName, "CMakeLists.txt"), "w")) as cmake:
+    cmake.writelines(cmakeContents)
